8483bed23f65c6602cb4679bc48ad33f91705af0
medfahmy <medfahmy99@gmail.com>
2021-12-13 16:12:23 +0100

    initial commit

---
+++ pun.c
@@ -0,0 +1,13 @@
+/*********************************************
+*   Name: pun.c                              *
+*   Purpose: prints a bad pun                *
+*   Author: Med Fahmy                        *
+*   Date: Mon 13/12/2021                     *
+*********************************************/
+
+#include <stdio.h>
+
+int main(void) {
+    printf("to c or not to c, that is the question\n");
+    return 2;
+}
