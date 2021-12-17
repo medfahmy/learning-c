a0c0c9b9b4628e38fea65f9c159d0c55686b661c
medfahmy <medfahmy99@gmail.com>
2021-12-17 12:48:10 +0100

    types and formatting

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-build:
+main :
 	gcc src/main.c -O -Wall -W -pedantic -ansi -std=c99 -o bin/main
+
+format :
+	gcc src/format.c -o bin/format -O -Wall -W -pedantic -ansi -std=c99 && ./bin/format
--- src/no-init.c
+++ src/no-init.c
@@ -4,8 +4,8 @@ int main(void) {
     int a, b, c, d, e, f;
     float A, B, C, D, E, F;
 
-    printf("ints %d, %d, %d, %d, %d, %d", a, b, c, d, e, f);
-    printf("floats %.2f, %.2f, %.2f, %.2f, %.2f, %.2f", A, B, C, D, E, F);
+    printf("ints %d, %d, %d, %d, %d, %d\n", a, b, c, d, e, f);
+    printf("floats %.2f, %.2f, %.2f, %.2f, %.2f, %.2f\n", A, B, C, D, E, F);
 
     return 0;
 }
