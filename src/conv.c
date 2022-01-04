b3b96bd6d2dd834014396504ed5e74b526612f7d
medfahmy <medfahmy99@gmail.com>
2022-01-04 17:40:29 +0100

    update conv.c

--- src/conv.c
+++ src/conv.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int i = -10;
-    unsigned u = 10;
+    // int i = -10;
+    // unsigned u = 10;
 
-    int b = i < u;
-    printf("%d\n", b);
+    // int b = i < u;
+    // printf("%d\n", b);
+
+    char ch = '\xdb';
+    float f = 3.1E+5;
+    
+    printf("%c\n", ch);
+    printf("%f\n", f);
 
     return 0;
 }
