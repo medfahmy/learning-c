3dada00b35cab01a224ea4d998107cb3f9c2b60b
medfahmy <medfahmy99@gmail.com>
2022-01-05 12:06:55 +0100

    started chap 8: arrays

--- src/arr.c
+++ src/arr.c
@@ -1,27 +1,11 @@
 #include <stdio.h>
 
-struct Point {
-    int x, y;
-};
-
-struct Person {
-    char name[50];
-    int age;
-};
-
-static int max(int x, int y, int z) {
-    return (x > y && x > z) ? x : ((y > z) ? y : z);
-}
+#define N 10
 
 int main() {
-    struct Point p = {0, 1};
+    int a[N];
 
-    struct Person john = { "john doe", 27 } ;
-
-    printf("x: %d, y: %d\n", p.x, p.y);
-    printf("name: %s, age: %d\n", john.name, john.age);
-
-    printf("max(10, 20, 5) = %d\n", max(10, 20, 5));
+    printf("hello world\n");
 
     return 0;
 }
